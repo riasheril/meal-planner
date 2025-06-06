e1982203f440836c55a583848215d9d838654334
Spanda88 <shefalipandare@gmail.com>
2025-06-06 11:26:04 -0700

    feat: add navigation and protect meal plan route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -10,6 +10,7 @@ import Onboarding from "./pages/Onboarding";
 import Recipes from "./pages/Recipes";
 import MealPlan from "./pages/MealPlan";
 import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
 import { useAuth0 } from '@auth0/auth0-react';
 
 const queryClient = new QueryClient();
@@ -33,13 +34,23 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/onboarding" element={<Onboarding />} />
           <Route path="/recipes" element={<Recipes />} />
-          <Route path="/meal-plan" element={<MealPlan />} />
+          <Route 
+            path="/meal-plan" 
+            element={
+              isAuthenticated ? (
+                <MealPlan />
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            } 
+          />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
